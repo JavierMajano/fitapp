217acce03af724c72ce5292ee2bd5e4ef4f6aeee
Javier M <73079627+JavierMajano@users.noreply.github.com>
2026-04-15 21:23:10 -0400

    Fix string quotes in server index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,6 +38,6 @@ app.use(
     createContext,
   }),
 );
-app.listen(PORT, "0.0.0.0", function () {
+app.listen(PORT, '0.0.0.0', function () {
   console.warn(`[server] Running on port ${PORT}`);
 });
